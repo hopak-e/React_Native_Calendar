00f2fab418f6158d42a47e1a5b31b80d42e6e613
hopak-e <a328028@gmail.com>
2023-01-23 00:53:55 +0900

    📝 주,월 담은 상수 추가

---
+++ constants/Calendar.ts
@@ -0,0 +1,15 @@
+export const week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+export const Months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
